73fbda64043d84b50a14fcc8081fb0bf518f29bd
XyloRekindled <uprashanth94@outlook.com>
2018-04-12 17:35:04 +0530

    Add geolocation support

--- public/js/index.js
+++ public/js/index.js
@@ -16,14 +16,39 @@ socket.on('newMessage', function (message) {
     jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (position) {
+    var image = jQuery(`<img src="${position.url}"/>`);
+
+    var li = jQuery('<li></li>');
+    li.append(image);
+
+    jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
 
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: jQuery('[name=message]').val(),
     }, function() {
 
     });
+});
+
+var locationButton = jQuery('#send-location');
+
+    locationButton.on('click', function() {
+    if(!navigator.geolocation) { return alert('Geolocation not supported by your browser.')};
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        socket.emit('createLocationMessage', {
+            lat: position.coords.latitude,
+            lon: position.coords.longitude
+        });
+    }, function () {
+        alert('Unable to fetch location.');
+    });
+
 });
 
--- server/utils/message.js
+++ server/utils/message.js
@@ -2,4 +2,19 @@ var generateMessage = (from, text) => {
     return { from, text, created: new Date().getTime() };
 };
 
-module.exports = { generateMessage };
+var generateLocationMessage = (from, lat, lon) => {
+    
+    var geo = `${lat},${lon}`;
+    var mapSize = 18;
+    var sizeWidth = 250;
+    var sizeHeight = 200;
+    var mapString = `https://maps.googleapis.com/maps/api/staticmap?center=${geo}&zoom=${mapSize}&size=${sizeWidth}x${sizeHeight}&markers=red|${geo}&key=AIzaSyDQjBzueh78LfxK1J06s-NCz7VXfEn2c8o`;
+    
+    return { 
+        from,
+        url: mapString,
+        createdAt: new Date().getTime()
+    };
+};
+
+module.exports = { generateMessage, generateLocationMessage };
